72d7137f425b838ac227ea4eac92bd1f13601f46
Kenny Williams <kennyjwilli@gmail.com>
2020-09-19 11:44:53 -0700

    Update comments.

--- env/example.clj
+++ env/example.clj
@@ -21,6 +21,7 @@
 
 (comment
   (gen/sample (s/gen ::user) 5)
+  (gen/sample (s/gen ::company-addr) 5)
   )
 
 
@@ -44,7 +45,7 @@
                                   ::state  "IL" ::zip 12345}})
 
 (comment
-  (s/explain ::place-order {::first "Alex" ::last "Miller" ::addr {::state "IL"}})
+  (s/explain-data ::place-order {::first "Alex" ::last "Miller" ::addr {::state "IL"}})
   (gen/sample (s/gen ::place-order) 3)
   )
 
